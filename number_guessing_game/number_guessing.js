0678edd9c7f079d52648f34aad8fa62cf6c845c2
Ryan <ryantonypereira@gmail.com>
2025-11-03 20:39:54 -0600

    Improve number guessing game logic and messages
    Refactored the game to use a random number between 0 and 99, added input validation and feedback for guesses, and improved console messages for user interaction. The game now continues until the correct number is guessed and provides appropriate hints for higher or lower guesses.

--- number_guessing_game/number_guessing.js
+++ number_guessing_game/number_guessing.js
@@ -1,15 +1,39 @@
+
+//Lets a userNumberGuess be set to 0 for default
 let userNumberGuess = 0;
+//Lets a isNumberCorrect variable be set to false as default
 let isNumberCorrect = false;
-const randomNumber = Math.random();
+//Lets a constnat varaible randomNumber be a random number bewtween 0 and 99
+const randomNumber = Math.floor(Math.random() * 100);
 
 
-
+//Console Logs into the terminal a simple welcoming message
 console.log("Welcome to the Number Guessing Game");
-while(isNumberCorrect){
-    userNumberGuess = parseInt(prompt("Guess a Number: "));
-    if(userNumberGuess === randomNumber){
-        console.log()
-        isNumberCorrect = true;
 
+
+//While the isNumberCorrect is set to false, the system will constantly ask the user for a number guess, until the guess is correct, making the condition false
+while(isNumberCorrect == false){
+    //UserNumberGuess is set to the parsing of the string input from the user to integer
+    userNumberGuess = parseInt(prompt("Guess a Number: "));
+    //If the userNumber guess is equal to the randomNumber, the guess is correct
+    if(userNumberGuess === randomNumber){
+        //Logs into the console to the user they ahve geuss right
+        console.log("Congratulation you have guessed the correct number")
+        //The isNumberCorrect is set to true, which should return out of the while loop
+        isNumberCorrect = true;
+    }
+    //Else if the guess is larger than the randoNumber, than the guess is too larger
+    else if(userNumberGuess > randomNumber){
+        //Logs into the console that the number is too large
+        console.log("Sorry, your guess number is larger")
+    }
+    //Else if the guess is larger than the randoNumber, than the guess is too lesser
+    else if(userNumberGuess < randomNumber){
+        //Logs into the console that the number is too less
+        console.log("Sorry, your guess number is lesser")
     }
 }
+
+//Logs into the conole a simple goodbye message
+console.log("Thanks for playing");
+console.log("Goodbye");
