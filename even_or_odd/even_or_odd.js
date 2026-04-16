d31315674f7311a394aa20abee94367eef944ad3
Ryan <ryantonypereira@gmail.com>
2026-04-15 21:18:04 -0500

    Documentation
    Documenting the file for every line on purpose of it.

--- even_or_odd/even_or_odd.js
+++ even_or_odd/even_or_odd.js
@@ -13,26 +13,37 @@
 
 
 
-
+//Defined a function that takes a number to check and return a string to indicate if odd,even, or invalid.
 function is_even_or_odd(numberToCheck){
+    //Letting numberRemainder to numberToCheck with remainder of 2 to get even,odd,or invalid
     let numberRemainder = numberToCheck % 2;
+    //If numberRemainder is 0, then even
     if(numberRemainder === 0){
+        //Return even string
         return "Even"
     }
+    //If numberRemainder is 1, then odd
     if(numberRemainder === 1){
+        //Return odd string
         return "Odd"
     }
+
+    //If all conditions don't meet
     else{
+        //Return invalid string
         return "Invalid"
     }
 }
 
 
 
-
+//Defining the main function, the entry point into the program
 function main(){
+    //Let userNumber set to the prompt input from user, parsing it for the integer
     let userNumber = parseInt(prompt("Number: "))
+    //Reuslt is set to the result of the userNumber as parameter input into is_even_or_odd function
     let result = is_even_or_odd(userNumber)
+    //Logs into console with string and result in it to indciate to user if number is even,odd, or invalid.
     console.log("\nThe Number you put is " + result + ".")
 
     
@@ -44,5 +55,5 @@ function main(){
 
 
 
-
+//Calling the main function
 main();
