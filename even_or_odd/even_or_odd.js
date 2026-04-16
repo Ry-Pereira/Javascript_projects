cd19e9c910fbaea242cf5f7b5a12a44233ab0c8a
Ryan <ryantonypereira@gmail.com>
2026-04-15 21:09:13 -0500

    Writing Code
    Added javascript code

--- even_or_odd/even_or_odd.js
+++ even_or_odd/even_or_odd.js
@@ -4,3 +4,37 @@
 
 
 
+
+
+function is_even_or_odd(numberToCheck){
+    let numberRemainder = numberToCheck % 2;
+    if(numberRemainder === 0){
+        return "Even"
+    }
+    if(numberRemainder === 1){
+        return "Odd"
+    }
+    else{
+        return "Invalid"
+    }
+}
+
+
+
+
+function main(){
+    let userNumber = parseInt(prompt("Number: "))
+    let result = is_even_or_odd(userNumber)
+    console.log("\nThe Number you put is " + result + ".")
+
+    
+}
+
+
+
+
+
+
+
+
+main();
