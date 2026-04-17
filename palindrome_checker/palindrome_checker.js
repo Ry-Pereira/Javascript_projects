aeba3c016db8b87ed96faab6ff78cd61e0e18e2d
Ryan <ryantonypereira@gmail.com>
2026-04-17 17:20:41 -0500

    Debugging
    Debugging the entire javascript file.

--- palindrome_checker/palindrome_checker.js
+++ palindrome_checker/palindrome_checker.js
@@ -1,8 +1,8 @@
 //Name: Ryan Pereira
-//Project Name: Even or Odd
-//Description: A program that asks the user for a number and returns a string on whether the number is even or odd.
-//Module Name: even_or_odd.js
-//Module Purpose: Set up the main entry point of the program while getting user data and deciding whether the number is even, odd, or invalid.
+//Project Name: Palindrome Checker
+//Description: A program that asks the user for a input, assumed as string. Return a string indicating that its a palindrome, in that the word is the same if reversed.
+//Module Name: palindrome_checker.js
+//Module Purpose: Set up the main entry point of the program while getting user input to see if reversed is a palindrome or not.
 //Collaborators: None
 //Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
 //Date: 4/17/2026
@@ -11,14 +11,34 @@
 
 
 
+function isPalindrome(input,reversedInput){
+    if(input === reversedInput){
+        return "a Palindrome!"
+    }
+    else{
+        return "not a Palindrome!"
+    }
+}
 
 
-function isPalindrome(userInput){
+
+function reverseInput(userInput){
     var userInputLength = userInput.length;
+    var reverseUserInput = "";
     if(userInputLength <= 1){
-        return "Give a valid palindrome input";
+        return " a Invalid Palindrome input\nTry Again"
 
     }
+
+    else{
+        for(var index = userInputLength-1;index>=0;index--){
+            reverseUserInput += userInput[index];
+
+        }
+    }
+     return isPalindrome(userInput, reverseUserInput)
+
+
 
 }
 
@@ -28,7 +48,8 @@ function isPalindrome(userInput){
 function mainFunction(){
     console.log("Welcome to the Palindrome Checker\n");
     let userPalindromeInput = prompt("Input to Check for Palindrome: ");
-    isPalindromeResult = isPalindrome(userPalindromeInput);
+    isPalindromeAnswer = reverseInput(userPalindromeInput);
+    console.log(userPalindromeInput + " is " + isPalindromeAnswer)
 }
 
 
