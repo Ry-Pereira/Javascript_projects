fdaecb0201817ee05e1ecb376b6d65d1548a49ff
Ryan <ryantonypereira@gmail.com>
2026-04-17 13:54:42 -0500

    Created New Project
    Created New Project with prologue comment for palindrome checker.

---
+++ palindrome_checker/palindrome_checker.js
@@ -0,0 +1,16 @@
+//Name: Ryan Pereira
+//Project Name: Even or Odd
+//Description: A program that asks the user for a number and returns a string on whether the number is even or odd.
+//Module Name: even_or_odd.js
+//Module Purpose: Set up the main entry point of the program while getting user data and deciding whether the number is even, odd, or invalid.
+//Collaborators: None
+//Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+//Date: 4/17/2026
+//Last Modified: 4/17/2026
+
+
+
+
+
+
+
