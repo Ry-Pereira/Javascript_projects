fde316b2c1cc1ce6d4a6a02a6a0ecbb2ee52a62b
Ryan <ryantonypereira@gmail.com>
2026-04-17 13:47:41 -0500

    Debugging
    Put semicolons in code.

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -19,24 +19,24 @@ function fizzBuzzSelection(number){
         //If the number has 0 remainder after dividing by 5, then its FizzBuzz
         if(number%5 ===0){
             //Returns FizzBuzz string
-            return "FizzBuzz"
+            return "FizzBuzz";
         }
 
     }
     //If the number has 0 remainder after dividing by 3, then its Fizz
     if(number%3 === 0){
         //Returns Fizz string
-        return "Fizz"
+        return "Fizz";
     }
     //If the number has 0 remainder after dividing by 5, then its Buzz
     if(number%5 === 0){
         //Returns Buzz string
-        return "Buzz"
+        return "Buzz";
     }
     //Else if all conditions don't meet then the number does not qualify
     else{
         //Returns the number itsself
-        return number
+        return number;
     }
 }
 
@@ -46,16 +46,16 @@ function fizzBuzzSequence(numberStart,numberEnd){
     //If numberStart is greater than the numberEnd, then the sequence ends
     if(numberStart > numberEnd){
         //Returns out of sequence function
-        return
+        return;
     }
     //Else the numberStart has not met the numberEnd
     else{
         //FizzBuzzResult is set to the result of the function fizzBuzzSelection with numberStart passed in as argument input
-        fizzBuzzResult = fizzBuzzSelection(numberStart)
+        fizzBuzzResult = fizzBuzzSelection(numberStart);
         //Logs into console with numberstart,colon, and the fizzBuzzResult
-        console.log(numberStart + ":" + fizzBuzzResult)
+        console.log(numberStart + ":" + fizzBuzzResult);
         //Calls the function in itself, with numberStart incremented by 1, with numberEnd passed in, recursively.
-        fizzBuzzSequence(numberStart++,numberEnd)
+        fizzBuzzSequence(numberStart++,numberEnd);
     }
 }
 
@@ -63,17 +63,17 @@ function fizzBuzzSequence(numberStart,numberEnd){
 //Defining the mainFunction, the entry point into the program itself
 function mainFunction(){
     //Logs into the console a welcome message to user of the program
-    console.log("Welcome to the FIZZBUZZ Program\n")
+    console.log("Welcome to the FIZZBUZZ Program\n");
     //Lets userStartNumber be set to the prompted input from user that is parsed for the integer
-    let userStartNumber = parseInt(prompt("Start Number: "))
+    let userStartNumber = parseInt(prompt("Start Number: "));
     //Lets userEndNumber be set to the prompted input from user that is parsed for the integer
-    let userEndNumber = parseInt(prompt("End Number: "))
+    let userEndNumber = parseInt(prompt("End Number: "));
     //Calling the fizzBuzzSequence with userStartNumber and userEndNumber passed in as argument
-    fizzBuzzSequence(userStartNumber,userEndNumber)
+    fizzBuzzSequence(userStartNumber,userEndNumber);
     //Logs into the console a welcome message to user of the program
-    console.log("\nProgram has ended\nGoodbye")
+    console.log("\nProgram has ended\nGoodbye");
 }
 
 
 //Calling the main function, for whole program to begin.
-mainFunction()
+mainFunction();
