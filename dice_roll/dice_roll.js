453a7190c9689fe6094a11314830798548e4dfde
Ryan <ryantonypereira@gmail.com>
2026-04-18 18:56:37 -0500

    Writing Code
    Added code to javascript file. Added functionality to roll dice from range of 1 to 6. Then functionality to take user input to roll dice, until no longer.

--- dice_roll/dice_roll.js
+++ dice_roll/dice_roll.js
@@ -2,6 +2,46 @@
 
 
 
+function rollTheDice(){
+    diceValue = Math.floor(Math.random() * 7);
+    if(diceValue === 0){
+        return 1;
+    }
+    else{
+        return diceValue;
+    }
+}
 
 
 
+function askUserToRollDice(userAnswer){
+    while(userAnswer != "N"){
+        if(userAnswer === "Y"){
+            rolledDiceValue = rollTheDice();
+            console.log("Roll...Roll..Roll");
+            console.log("Dice Roll Value: " + rolledDiceValue);
+        }
+        else{
+            console.log("Please give a valid answer.");
+        }
+        userAnswer = prompt("Would you like to roll the dice(Y/N): ");
+    }
+
+    
+}
+
+
+
+function mainFunction(){
+    console.log("Welcome to the Dice Roll Program");
+    userAnswerToQuestion = prompt("Would you like to roll the dice(Y/N): ");
+    askUserToRollDice(userAnswerToQuestion);
+    console.log("\nThe program has ended...");
+
+
+}
+
+
+
+
+mainFunction();
