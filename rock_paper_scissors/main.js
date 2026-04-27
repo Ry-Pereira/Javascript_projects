fa158cc7c1c66907465140c29a47bbac3dfda7af
Ryan <ryantonypereira@gmail.com>
2026-04-27 17:32:00 -0500

    Add rock-paper-scissors CLI game
    Introduce a simple 3-round Rock-Paper-Scissors game (rock_paper_scissors/main.js). Adds move list and functions to determine round winner (whoWonRound), tally and announce the game winner (determineGameWinner), run rounds with prompt-driven user input and random computer moves (gameRun), and an entrypoint (mainFunction). Invalid user moves are treated as a lost round; results and scores are printed to the console.

--- rock_paper_scissors/main.js
+++ rock_paper_scissors/main.js
@@ -1,9 +1,73 @@
+var moves = ["Rock","Paper","Scissors"];
 
+function whoWonRound(userMoveToJudge, computerMoveToJudge) {
+    if (userMoveToJudge !== "Rock" && userMoveToJudge !== "Paper" && userMoveToJudge !== "Scissors") {
+        return "Invalid Move";
+    }
+    if (userMoveToJudge === computerMoveToJudge) {
+        return "Tie";
+    }
+    if (userMoveToJudge === "Rock" && computerMoveToJudge === "Scissors") {
+        return "User";
+    }
+    if (userMoveToJudge === "Paper" && computerMoveToJudge === "Rock") {
+        return "User";
+    }
+    if (userMoveToJudge === "Scissors" && computerMoveToJudge === "Paper") {
+        return "User";
+    }
+    return "Computer";
+}
 
+function determineGameWinner(userGameScore, computerGameScore){
+    if(userGameScore === computerGameScore){
+        console.log("Game Winner: No one, its a tie");
+    }
+    else if(userGameScore > computerGameScore){
+        console.log("Game Winner: User Wins");
+    }
+    else{
+        console.log("Game Winner: Computer Wins");
+    }
+}
 
+function gameRun(){
+    var userScore = 0;
+    var computerScore = 0;
 
+    for (let gameRound = 0 ; gameRound < 3; gameRound++){
+        console.log("Round: " + (gameRound+1));
+        let userMove = prompt("User Move: ");
+        let computerMove = moves[Math.floor(Math.random() * moves.length)];
 
+        let result = whoWonRound(userMove,computerMove);
 
+        if(result == "Invalid Move"){
+            console.log("For Invalid Input, you will lose this round");
+            computerScore++;
+        }
+        else if(result == "Tie"){
+            console.log("\nUser Move: " + userMove + "\nComputer Move: " + computerMove + "\nResult Tie");
+        }
+        else if(result == "User"){
+            console.log("\nUser Move: " + userMove + "\nComputer Move: " + computerMove + "\nResult: User Wins Round");
+            userScore ++;
+        }
+        else{
+            console.log("\nUser Move: " + userMove + "\nComputer Move: " + computerMove + "\nResult: Computer Wins Round");
+            computerScore ++;
+        }
+    }
 
+    determineGameWinner(userScore,computerScore);
+}
 
+function mainFunction(){
+    console.log("Welcome to the Rock Paper Scissors Game!");
+    gameRun();
+    console.log("Program has ended");
+}
 
+if(require.main === module){
+    mainFunction();
+}
