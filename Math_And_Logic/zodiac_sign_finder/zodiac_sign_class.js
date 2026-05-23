e6f14e1d6afc18b66e636dbd37879853cfe4d52f
Ryan <ryantonypereira@gmail.com>
2026-05-23 16:27:42 -0500

    ZodiacSign Class Creation
    Created a ZodiacSign class for the purpose of being able to create a Zodiac Sign object.

---
+++ Math_And_Logic/zodiac_sign_finder/zodiac_sign_class.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+class ZodiacSign {
+    constructor(name, dateRange, element, traitsDescription) {
+        this.name = name;
+        this.dateRange = dateRange;
+        this.element = element;
+        this.traitsDescription = traitsDescription;
+    }
+}
