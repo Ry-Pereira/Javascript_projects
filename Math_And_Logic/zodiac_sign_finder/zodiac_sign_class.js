d6fc59e016ebdeb3f3e7eb7fb5b7df93fe67ab1b
Ryan <ryantonypereira@gmail.com>
2026-05-24 15:28:15 -0500

    Documentation
    Added documentation for the entire code document. Added comments for every single line of code in the document to explain purpose and how it contributes to the overall flow of the program. Added prologue comments to provided meta for code.

--- Math_And_Logic/zodiac_sign_finder/zodiac_sign_class.js
+++ Math_And_Logic/zodiac_sign_finder/zodiac_sign_class.js
@@ -1,16 +1,32 @@
+//Name: Ryan Pereira
+//Project Name: Zodiac Sign Finder
+//Description: A program that asks the user for a birth date and determines their zodiac sign.
+//Module Name: zodiac_sign_class.js
+//Module Purpose: Set up the ZodiacSign class which will be used to create objects for each zodiac sign with their respective information and traits.
+//Collaborators: None
+//Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+//Date: 5/23/2026
+//Last Modified: 5/24/2026
 
 
 
-
-
+// Exports the ZodiacSign class so it can be used in other files
 export class ZodiacSign {
+    // Constructor method used to create a new ZodiacSign object
     constructor(name, dateMonthStart, dateMonthEnd, dateDayStart, dateDayEnd, element, traitsDescription) {
+        // Stores the zodiac sign name
         this.name = name;
+        // Stores the starting month of the zodiac sign
         this.dateMonthStart = dateMonthStart;
+        // Stores the ending month of the zodiac sign
         this.dateMonthEnd = dateMonthEnd;
+        // Stores the starting day of the zodiac sign
         this.dateDayStart = dateDayStart;
+        // Stores the ending day of the zodiac sign
         this.dateDayEnd = dateDayEnd;
+        // Stores the zodiac sign element (Fire, Water, Earth, Air)
         this.element = element;
+        // Stores the personality traits description of the zodiac sign
         this.traitsDescription = traitsDescription;
     }
 }
